b7236f4ce553652b17833f8dc6ab9cac35df50b9
Christopher Wong <cwong51799@gmail.com>
2020-11-29 22:43:12 -0500

    Updates

---
+++ precache-manifest.8721a19f6b66e42d4bd8163ff04222a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "320db9f690296c01c182b480b70b0159",
+    "url": "/lunar-app/index.html"
+  },
+  {
+    "revision": "eea0d08f9b14d40cbdfb",
+    "url": "/lunar-app/static/css/main.8b1c35ed.chunk.css"
+  },
+  {
+    "revision": "1070c4dc551cc864b4be",
+    "url": "/lunar-app/static/js/2.97b1fbc4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/lunar-app/static/js/2.97b1fbc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eea0d08f9b14d40cbdfb",
+    "url": "/lunar-app/static/js/main.3f8adfdc.chunk.js"
+  },
+  {
+    "revision": "3b3d76ccbadfea04ff1b",
+    "url": "/lunar-app/static/js/runtime-main.da058900.js"
+  }
+]);
